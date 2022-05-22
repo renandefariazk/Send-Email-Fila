a8b21e55471a676ac52a0bb5b10296402168d193
Renan de Faria <renandefaria2009@hotmail.com>
2022-05-22 20:09:08 -0300

    Adicionando a Port do Projeto em uma Variavel env

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const emailRoute = require("./src/routes/emailRoute");
+require("dotenv").config();
 
-const port = 8000;
+const port = process.env.PORT || 8000;
+
 app.use(express.json());
 
 app.use("/", emailRoute);
